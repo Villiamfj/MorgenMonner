635c8969145f29618398ace3cb9cd26ed76e540e
Villiamfj <villiamfj@hotmail.dk>
2022-02-14 09:47:10 +0100

    Monner update RIP hamilton

--- MonnerGen.js
+++ MonnerGen.js
@@ -3,7 +3,6 @@ const monners = [
     {name:"MULE",image:"https://marvel-b1-cdn.bc0a.com/f00000000104050/www.monsterenergy.com/media/uploads_image/2021/10/04/auto/800/6ab304dbafc7650e81b3b6de410332b7.png?mod=v1_89b31e9371d86dc6e4d96e3d80218cbd"},
     {name:"ABSOLUTELY ZERO",image:"https://marvel-b1-cdn.bc0a.com/f00000000104050/www.monsterenergy.com/media/uploads_image/2016/05/30/auto/800/789a69dbf8d1cd184227a914653fae85.png?mod=v1_eb16e0188b13655f6d7d5a2346226550"},
     {name:"THE DOCTOR",image: "https://marvel-b1-cdn.bc0a.com/f00000000104050/www.monsterenergy.com/media/uploads_image/2020/11/25/auto/800/5f6f4f7d9e8903e988fcd41fa09fec9f.png?mod=v1_7c24b94e1e2d6c8d0f4d7fea76590c1b"},
-    {name:"HAMILTON", image:"https://marvel-b1-cdn.bc0a.com/f00000000104050/www.monsterenergy.com/media/uploads_image/2018/05/10/auto/800/fd74cdb2bf3467cd78d1ca0a4d9ab29d.png?mod=v1_3fb90284f50ff17c71de3ff0490812de"},
     {name:"ULTRA PARADISE",image: "https://marvel-b1-cdn.bc0a.com/f00000000104050/www.monsterenergy.com/media/uploads_image/2020/07/06/auto/800/fc9ea2b2525da898cdb0cd418bb6dc45.png?mod=v1_7bc1a34855bd9b67f0b3aed3da06821f"},
     {name:"ULTRA",image: "https://marvel-b1-cdn.bc0a.com/f00000000104050/www.monsterenergy.com/media/uploads_image/2016/10/05/auto/800/cd3a532014dc28421933f426d921f6da.png?mod=v1_da6f0dde227e368327ddd3b1dc23409c"},
     {name:"ULTRA VIOLET", image:"https://marvel-b1-cdn.bc0a.com/f00000000104050/www.monsterenergy.com/media/uploads_image/2019/03/26/auto/800/063b8eac1a68fb111d497c3c6fb984c7.png?mod=v1_e8eff231a4765e81431b7ba26c074002"},
@@ -11,7 +10,8 @@ const monners = [
     {name:"PACIFIC PUNCH", image:"https://marvel-b1-cdn.bc0a.com/f00000000104050/www.monsterenergy.com/media/uploads_image/2021/03/17/auto/800/ec4aefd568154202aee1d405b327aaeb.png?mod=v1_ec1b2610582e4feacef7cd11742ec5bb"},
     {name:"PIPELINE PUNCH", image:"https://marvel-b1-cdn.bc0a.com/f00000000104050/www.monsterenergy.com/media/uploads_image/2020/03/05/auto/800/5c815ea59fda9af66ebb1e9c5f2d8039.png?mod=v1_85981f86f457c447a176a6cb55b6e24a"},
     {name:"PUNCH", image: "https://marvel-b1-cdn.bc0a.com/f00000000104050/www.monsterenergy.com/media/uploads_image/2020/04/20/auto/800/b8a6ce93b2888a8003cfdd8b4f4ebdc2.png?mod=v1_6c0a12b713a933c28b654fc9b0e5449c"},
-    {name:"ULTRA FIESTA MANGO", image: "https://marvel-b1-cdn.bc0a.com/f00000000104050/www.monsterenergy.com/media/uploads_image/2021/09/15/auto/800/dffd857b8f083ab2f9a09a29b3f2659f.png?mod=v1_c7563dba853f15d53d67f5bc77d87e54"}
+    {name:"ULTRA FIESTA MANGO", image: "https://marvel-b1-cdn.bc0a.com/f00000000104050/www.monsterenergy.com/media/uploads_image/2021/09/15/auto/800/dffd857b8f083ab2f9a09a29b3f2659f.png?mod=v1_c7563dba853f15d53d67f5bc77d87e54"},
+    {name:"MONACH", image: "https://marvel-b1-cdn.bc0a.com/f00000000104050/www.monsterenergy.com/media/uploads_image/2021/04/28/auto/800/201063f5686f36a2a612862b433b43ee.png?mod=v1_82024746902c3f79b4a91783471d1d7e"}
 ]
 
 //inserting optionals
